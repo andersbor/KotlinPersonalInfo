1770f69d006b05a9a7d61b6960f47e252309bfce
Anders Børjesson <anbo@zealand.dk>
2019-03-12 09:44:37 +0100

    init

---
+++ app/src/main/java/dk/easj/anbo/personalinformation2/MainActivity.kt
@@ -0,0 +1,36 @@
+package dk.easj.anbo.personalinformation2
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val REQUEST_CODE_PERSONAL_INFO = 14593
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainInfoButton.setOnClickListener {
+            val intent = Intent(this, PersonalInfoActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_PERSONAL_INFO)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_CANCELED) {
+            mainMessageTextView.text = "Cancelled"
+            Log.d("MINE", "Cancelled")
+        } else {
+            val name = data?.getStringExtra(NAME)
+            val age = data?.getIntExtra(AGE, 0)
+            mainMessageTextView.text = ("Name: $name Age: $age")
+            Log.d("MINE", data?.getStringExtra(NAME))
+        }
+    }
+}
---
+++ app/src/main/java/dk/easj/anbo/personalinformation2/PersonalInfoActivity.kt
@@ -0,0 +1,42 @@
+package dk.easj.anbo.personalinformation2
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_personal_info.*
+
+const val NAME = "name"
+const val AGE = "age"
+
+class PersonalInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_info)
+
+        personalCancelButton.setOnClickListener {
+            val result = Intent()
+            setResult(Activity.RESULT_CANCELED, result)
+            finish()
+        }
+
+        personalSendButton.setOnClickListener {
+            val result = Intent()
+            result.putExtra(NAME, personalInfoNameEditText.text.toString())
+            try {
+                result.putExtra(AGE, personalInfoAgeEditText.text.toString().toInt())
+                setResult(Activity.RESULT_OK, result)
+                finish()
+            } catch (ex: NumberFormatException) {
+                personalInfoAgeEditText.error = "Not a number"
+            }
+        }
+
+        personalInfoNameEditText.setOnLongClickListener {
+            Log.d("MINE", "Long click")
+            true
+        }
+    }
+}
